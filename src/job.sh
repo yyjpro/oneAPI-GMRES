80fadec604e2c4fd775e70480fb8777f5ea2faf3
yyjpro <76054095+yyjpro@users.noreply.github.com>
2022-04-25 17:29:17 +0800

    Update job.sh

--- src/job.sh
+++ src/job.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dpcpp -fintelfpga main.cpp read_mm.cpp gmres.cpp scaling.cpp parallel_product.cpp -o fpga -Xsharedware -DFPGA=1
+dpcpp -fintelfpga main.cpp read_mm.cpp gmres.cpp scaling.cpp parallel_product.cpp -o fpga -Xshardware -DFPGA=1
